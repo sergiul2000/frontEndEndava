dfce57f210111faafbbfd4daab9e1f427d8bfda1
Lupsa Sergiu <sergiul2000@yahoo.com>
2022-08-05 11:30:16 +0300

    hamburger meniu de incorporat in paginile existente

---
+++ HamburgerMenu.js
@@ -0,0 +1,5 @@
+let navWrapper = document.querySelector('.navigationBar'), navToogler = document
+.querySelector('.navigationToggle')
+navToogler.addEventListener('click', function(event) {
+navWrapper.classList.toggle('active')
+});
--- studies.js
+++ studies.js
@@ -1,10 +1,11 @@
 const navDivs = document.querySelectorAll('navigationDiv');
 function handleOnClickIcon(){
+    //console.log(navDivs);
+
     navDivs.forEach(function(value){
     value.style.display = 'none';
-    //console.log(navDivs);
     })
-};
+}
 
 
 

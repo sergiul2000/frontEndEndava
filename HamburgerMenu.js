13aef6b427304fd62c6614b8b24f8c10fbe7dfa1
Lupsa Sergiu <sergiul2000@yahoo.com>
2022-08-05 11:34:42 +0300

    HamburgerMenu.js deleted

--- HamburgerMenu.js
+++
@@ -1,5 +0,0 @@
-let navWrapper = document.querySelector('.navigationBar'), navToogler = document
-.querySelector('.navigationToggle')
-navToogler.addEventListener('click', function(event) {
-navWrapper.classList.toggle('active')
-});

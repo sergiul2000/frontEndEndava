b1da351c0584b9d56e5c2723daa3a9f8f2300ab1
Lupsa Sergiu <sergiul2000@yahoo.com>
2022-08-10 10:26:54 +0300

    demos cu todo si aranjat inpagina

--- demos.js
+++ demos.js
@@ -12,3 +12,49 @@ meniuIcon.addEventListener('click',() =>{
         navigationList.classList.add('hidden');
     }
 });
+
+// cod pentru todos
+let todoItems = [];
+
+function renderTodo(todo) {
+  const list = document.querySelector('.js-todo-list');
+
+  const isChecked = todo.checked ? 'done': '';
+  const node = document.createElement("li");
+  node.setAttribute('class', `todo-item ${isChecked}`);
+  node.setAttribute('data-key', todo.id);
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+  list.append(node);
+}
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  renderTodo(todo);
+}
+
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input = document.querySelector('.js-todo-input');
+
+  const text = input.value.trim();
+  if (text != '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});

46a5629d43bda3362a92f751d61fa13e33a06c5d
Lupsa Sergiu <sergiul2000@yahoo.com>
2022-08-10 18:40:52 +0300

    proiect prezentabil cu darkMode

--- contacts.js
+++ contacts.js
@@ -12,3 +12,41 @@ meniuIcon.addEventListener('click',() =>{
         navigationList.classList.add('hidden');
     }
 });
+
+
+// --purple-dark-2: hsl(270, 0%, 5%);
+// --purple-dark-1: hsl(270, 20%, 15%);
+// --purple: hsl(270, 40%, 50%);
+// --purple-light-1: hsl(270, 40%, 55%);
+
+
+let root = document.querySelector(':root');
+
+function lightMode(){
+  root.style.setProperty('--purple-dark-1', '#FAFAFA');//background
+  root.style.setProperty('--purple-dark-2', '#8AA29E');//navigation color
+  root.style.setProperty('--purple', 'black');//writting
+  root.style.setProperty('--purple-light-1', '#E3F2FD');
+}
+
+function darkMode(){
+  root.style.setProperty('--purple-dark-1', 'hsl(270, 20%, 15%)');//background
+  root.style.setProperty('--purple-dark-2', 'hsl(270, 0%, 5%)');//navigation color
+  root.style.setProperty('--purple', 'hsl(270, 40%, 50%)');//writting
+  root.style.setProperty('--purple-light-1', 'hsl(270, 40%, 55%)');
+}
+
+const switchDarkMode = document.getElementById('switchInput');
+let darkModeIsChecked =true;
+
+switchDarkMode.addEventListener('click', () => {
+    if(darkModeIsChecked){
+        lightMode();
+        darkModeIsChecked=false;
+    }
+    else{
+        
+        darkMode();
+        darkModeIsChecked=true;
+    }
+});
--- demos.js
+++ demos.js
@@ -15,6 +15,7 @@ meniuIcon.addEventListener('click',() =>{
 
 // cod pentru todos
 let todoItems = [];
+let nrTodos =0;
 
 function renderTodo(todo) {
   const list = document.querySelector('.js-todo-list');
@@ -27,11 +28,10 @@ function renderTodo(todo) {
     <input id="${todo.id}" type="checkbox"/>
     <label for="${todo.id}" class="tick js-tick"></label>
     <span>${todo.text}</span>
-    <button class="delete-todo js-delete-todo">
+    <button id='butonDeleteCreated' class="delete-todo js-delete-todo">
     <svg><use href="#delete-icon"></use></svg>
     </button>
   `;
-
   list.append(node);
 }
 
@@ -57,4 +57,42 @@ form.addEventListener('submit', event => {
     input.value = '';
     input.focus();
   }
+});
+
+
+// --purple-dark-2: hsl(270, 0%, 5%);
+// --purple-dark-1: hsl(270, 20%, 15%);
+// --purple: hsl(270, 40%, 50%);
+// --purple-light-1: hsl(270, 40%, 55%);
+
+
+let root = document.querySelector(':root');
+
+function lightMode(){
+  root.style.setProperty('--purple-dark-1', '#FAFAFA');//background
+  root.style.setProperty('--purple-dark-2', '#8AA29E');//navigation color
+  root.style.setProperty('--purple', 'black');//writting
+  root.style.setProperty('--purple-light-1', '#DB5461');
+}
+
+function darkMode(){
+  root.style.setProperty('--purple-dark-1', 'hsl(270, 20%, 15%)');//background
+  root.style.setProperty('--purple-dark-2', 'hsl(270, 0%, 5%)');//navigation color
+  root.style.setProperty('--purple', 'hsl(270, 40%, 50%)');//writting
+  root.style.setProperty('--purple-light-1', 'hsl(270, 40%, 55%)');
+}
+
+const switchDarkMode = document.getElementById('switchInput');
+let darkModeIsChecked =true;
+
+switchDarkMode.addEventListener('click', () => {
+    if(darkModeIsChecked){
+        lightMode();
+        darkModeIsChecked=false;
+    }
+    else{
+        
+        darkMode();
+        darkModeIsChecked=true;
+    }
 });
